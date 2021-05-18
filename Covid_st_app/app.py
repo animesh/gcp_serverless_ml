2771e1e989099b1f36a1844c5384d7637d03e6e4
fuzzylife <animesh@fuzzylife.org>
2021-05-18 08:25:37 +0000

    update model with indicators cough, fever, sore throat, shortness of breath, headache

--- Covid_st_app/app.py
+++ Covid_st_app/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-model = pickle.load(open('./covidpred_v1.pkl','rb'))
+model = pickle.load(open('./covidpred_model_v1.pkl','rb'))
 
 def main():
 
@@ -11,16 +11,16 @@ def main():
 
     st.subheader('Trying to develop an Artificial Intelligence powered system to augment doctors in interpreting symptoms w.r.t. demography backed by data, probabilistic diagnosis, specifically, interpret lab tests and imaging results coming from suspected infectious disease and risk assessment of other conditions e.g. cancer, able to integrate into available health infrastructure via API(s), locally sourced data, predicting an outbreak, aiding resource allocation, add Bhojpuri/Hindi to aid urban and rural health care providers respectively. If you would like to get involved, please contact [animesh@fuzzylife.org]')
 
-    X_signght   = st.text_input('X sign', 0)
-    X_symptom    = st.text_input('X symptom', 0)
-    Y_signgth   = st.text_input('Y sign', 0)
-    Y_symptom    = st.text_input('Y symptom', 0)
-    Z_symptom    = st.text_input('Z symptom', 0)
+    X_signght   = st.text_input('cough', 0)
+    X_symptom    = st.text_input('fever', 0)
+    Y_signgth   = st.text_input('sore_throat', 0)
+    Y_symptom    = st.text_input('shortness_of_breath', 0)
+    Z_symptom    = st.text_input('headache', 0)
 
 
     if st.button('Get Prediction'):
 
-        pred = model.predict(np.array([[float(X_signght),float(X_symptom),float(Y_signgth),float(Y_symptom)]],float(Z_symptom)]]))
+        pred = model.predict(np.array([[float(X_signght),float(X_symptom),float(Y_signgth),float(Y_symptom),float(Z_symptom)]]))
 
         st.text('Predicted Z:' + pred[0])
 
